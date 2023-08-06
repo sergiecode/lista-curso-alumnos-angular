5f6a08417b2c4a0eb330217d78409269d54d38a8
SergieCode <sergiosnsdk@gmail.com>
2023-08-05 21:57:05 -0300

    Tablas en Boostrap de alumnos y cursos para practicar Typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lista-cursos';
+
+  cursos:boolean = true
+
+  handleCambio(){
+    this.cursos = !this.cursos
+  }
+
 }
---
+++ src/app/components/tabla-alumnos/mock.ts
@@ -0,0 +1,19 @@
+import { Alumno } from "src/app/models/alumno-to";
+
+export const alumnos: Alumno[] = [
+{
+    nombre: 'Ricardo',
+    apellido: 'Perez',
+    promedio: 8.5
+},
+{
+    nombre: 'Florencia',
+    apellido: 'Gonzalez',
+    promedio: 9
+},
+{
+    nombre: 'Pedro',
+    apellido: 'Francella',
+    promedio: 6
+}
+]
